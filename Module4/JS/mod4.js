8206d56af2ecb5cb1d8b2776187c0b034a05a598
AshRobFielder <ashrobfielder@gmail.com>
2024-08-08 13:45:10 +0100

    Change to structure

---
+++ Module4/JS/mod4.js
@@ -0,0 +1,14 @@
+var message = "in global";
+console.log("global: message")
+
+// var a = function () {
+// var message = "inside a"
+// console.log("a: message")
+// b();
+// }
+
+// function b () {
+// console.log("b:message")
+// }
+
+// a();
