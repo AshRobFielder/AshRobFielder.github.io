c29cc7541624b5d3fde9187aeaa25f822aaa0570
AshRobFielder <ashrobfielder@gmail.com>
2024-08-08 22:48:07 +0100

    mod4v1.4

--- Module4/JS/SpeakHello.js
+++ Module4/JS/SpeakHello.js
@@ -1,16 +1,15 @@
 // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
-(function (window) {
+(function () {
   var helloSpeaker = {};
   var speakWord = "Hello";
-  helloSpeaker.speak = function () {
-    console.log(speakWord);
-  }
-
-  window.helloSpeaker = helloSpeaker
-
-})(window);
+   helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
+}
+());
 
 // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
 // the "speak" method and which you will expose to the global context
